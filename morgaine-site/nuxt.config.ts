b559fb07eb42449aa80ff033e1d35acfc31df88f
Ethan Brockman <158540194+EthanB87@users.noreply.github.com>
2024-11-22 21:57:14 -0500

    tailwind config

--- morgaine-site/nuxt.config.ts
+++ morgaine-site/nuxt.config.ts
@@ -3,7 +3,14 @@ import Aura from '@primevue/themes/aura';
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
+  css: ['~/assets/css/main.css'],
   modules: ['@nuxtjs/tailwindcss', '@nuxt/eslint', '@primevue/nuxt-module'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
   primevue: {
     options: {
         theme: {
